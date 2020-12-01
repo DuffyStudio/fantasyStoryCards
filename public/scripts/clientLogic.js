ab90977367c546b8f37680b659b8360b1aec55b2
Chris Duffy <christopher.b.duffy@gmail.com>
2020-11-30 17:41:46 -0800

    adding an html5 canvas

--- public/scripts/clientLogic.js
+++ public/scripts/clientLogic.js
@@ -1,5 +1,6 @@
 var pName = prompt("Enter your name");
 socket.emit('playerName',pName);
+
 var messageFactory = {};
 messageFactory.makeMSG = function(string){
     var data = {};
@@ -12,3 +13,14 @@ function postMessage(){
     var data = messageFactory.makeMSG(str);
     socket.emit('msg',data);
 }
+
+function draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    for (var i = 0; i < 6; i++) {
+      for (var j = 0; j < 6; j++) {
+        ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
+                         Math.floor(255 - 42.5 * j) + ', 0)';
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+      }
+    }
+  }
